dfa9678b072f0dd6ee5eaa1cfa79fb49924dd86c
Gitmiekki <pierre.doignies.etu@univ-lille.fr>
2021-06-28 15:43:19 +0200

    first working commit

--- NaiveIndex.h
+++ NaiveIndex.h
@@ -18,17 +18,7 @@ using namespace std;
 
 class NaiveIndex{
 public:
-    int64_t nb_minimizer;
-    int64_t nb_genomes;
-    vector<vector<uint8_t>> matrix;//Column=Partitions line=genomes
-
-    NaiveIndex NaiveIndex(int64_t n);
-    void add_sequence(const string& str);
-    vector<double> query_sequence(const string& str);
-
-    void add_sketch(const vector<uint8_t>& V);
-    vector<uint8_t> compute_sketch(const string& str);
-    uint8_t hash(const string& str);
+     int64_t number_indexes;
 
 };
 
