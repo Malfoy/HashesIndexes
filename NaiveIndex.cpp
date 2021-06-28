3a26f0fe05549d472fa575799094a39816533ca2
Gitmiekki <pierre.doignies.etu@univ-lille.fr>
2021-06-28 17:11:31 +0200

    begin compute_sketch

--- NaiveIndex.cpp
+++ NaiveIndex.cpp
@@ -3,17 +3,47 @@
 #include <iostream>
 #include <vector>
 #include <atomic>
+#include <functional>
 #include <mutex>
+#include <string>
 #include <stdint.h>
+#include <math.h>
 #include "NaiveIndex.h"
+
 
 
 using namespace std;
 
 
 
+hash<std::string> str_hash;
+
+
+
 NaiveIndex::NaiveIndex(int64_t n){
   matrix.resize(n);
   nb_minimizer=n;
+  bit_to_keep_minimizer=log2(n);
   nb_genomes=0;
 };
+
+
+
+uint64_t NaiveIndex::get_bucket(int64_t h){
+  return h>>(64-bit_to_keep_minimizer);
+}
+
+
+
+uint8_t NaiveIndex::get_hash(int64_t h){
+  return h%256;
+}
+
+
+
+vector<uint8_t> NaiveIndex::compute_sketch(const string& str){
+  vector<uint8_t> result(nb_minimizer,255);
+
+
+  return result;
+}
--- NaiveIndex.h
+++ NaiveIndex.h
@@ -20,6 +20,7 @@ class NaiveIndex{
 public:
       int64_t nb_minimizer;
       int64_t nb_genomes;
+      int64_t bit_to_keep_minimizer;
       vector<vector<uint8_t>> matrix;//Column=Partitions line=genomesint64_t number_indexes;
       NaiveIndex(int64_t n);
       void add_sequence(const string& str);
@@ -27,7 +28,9 @@ public:
 
       void add_sketch(const vector<uint8_t>& V);
       vector<uint8_t> compute_sketch(const string& str);
-      uint8_t hash(const string& str);
+      uint8_t hash_str(const string& str);
+      uint64_t get_bucket(int64_t h);
+      uint8_t get_hash(int64_t h);
 };
 
 
