798c3854bc8ca1c3c340bea5ac3246c5c3103269
Gitmiekki <pierre.doignies.etu@univ-lille.fr>
2021-06-29 16:27:19 +0200

    for loop compute sketch

--- NaiveIndex.cpp
+++ NaiveIndex.cpp
@@ -16,34 +16,54 @@ using namespace std;
 
 
 
-hash<std::string> str_hash;
+hash<std::string> kmerHasher;
 
 
 
-NaiveIndex::NaiveIndex(int64_t n){
+NaiveIndex::NaiveIndex(int64_t n) //index constructor
+{
   matrix.resize(n);
   nb_minimizer=n;
   bit_to_keep_minimizer=log2(n);
   nb_genomes=0;
+  kmerSize=31;
 };
 
 
 
-uint64_t NaiveIndex::get_bucket(int64_t h){
-  return h>>(64-bit_to_keep_minimizer);
+uint64_t NaiveIndex::get_bucket(int64_t h)
+{
+  return h>>(64-bit_to_keep_minimizer); //bit shift to know bucket, first bits, most significant bit (MSB)
 }
 
 
 
-uint8_t NaiveIndex::get_hash(int64_t h){
-  return h%256;
+uint8_t NaiveIndex::get_hash(int64_t h)
+{
+  return h%256;  //to obtain the true hash, last bits, least significant bit (LSB)
 }
 
 
 
-vector<uint8_t> NaiveIndex::compute_sketch(const string& str){
-  vector<uint8_t> result(nb_minimizer,255);
+vector<uint8_t> NaiveIndex::compute_sketch(const string& sequenceStr,const int kmerSize)
+{
+  vector<uint8_t> result(nb_minimizer,255); // the resulting vector
 
+  // a For loop to hash every kmer of a sequence, distribute them in different
+  // bucket with their MSB on one byte (255 buckets) and record some LSB number
+  // of the minimal hash least
+
+  int sequenceSize(sequenceStr.size()), counter(0);
+
+  for (int position = 0 ; position < (sequenceSize - kmerSize) ; position++)
+  {
+    string kmerToHash(sequenceStr.substr (position,kmerSize));
+    int64_t hashOfKmer(kmerHasher(kmerToHash));
+    if(result[get_bucket(hashOfKmer)] > get_hash(hashOfKmer))
+    {
+      result[get_bucket(hashOfKmer)] =  get_hash(hashOfKmer);
+    }
+  }
 
   return result;
 }
--- NaiveIndex.h
+++ NaiveIndex.h
@@ -21,13 +21,14 @@ public:
       int64_t nb_minimizer;
       int64_t nb_genomes;
       int64_t bit_to_keep_minimizer;
+      int64_t kmerSize;
       vector<vector<uint8_t>> matrix;//Column=Partitions line=genomesint64_t number_indexes;
       NaiveIndex(int64_t n);
       void add_sequence(const string& str);
       vector<double> query_sequence(const string& str);
 
       void add_sketch(const vector<uint8_t>& V);
-      vector<uint8_t> compute_sketch(const string& str);
+      vector<uint8_t> compute_sketch(const string& sequenceStr,const int kmerSize);
       uint8_t hash_str(const string& str);
       uint64_t get_bucket(int64_t h);
       uint8_t get_hash(int64_t h);
