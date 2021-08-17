7554a04a72d98caaca282b30b50311396569a257
Pimiso <78482038+Pimiso@users.noreply.github.com>
2021-08-17 15:55:31 +0200

    better print for result evaluation

--- EvaluationMaker.cpp
+++ EvaluationMaker.cpp
@@ -51,12 +51,17 @@ void ComparisonMatrix::create_comparison()
         uint32 genomeIndex(0);
         for (methodLine = 1; methodLine < testMatrix.size(); methodLine++)
         {
-                vector<uint32> differentScores(6);//sum the corresponding result at his category (different score[1] = success score)
+                vector<uint32> differentScores(7);//sum the corresponding result at his category (different score[1] = success score)
                 for (genomeIndex = 0; genomeIndex < matrixHeight; genomeIndex++)
                 {
                         if (testMatrix[0][genomeIndex] == testMatrix[methodLine][genomeIndex])// if it's the same Jaccard index or both 0 it's a success
                         {
                                 differentScores[1]++;
+                                if (testMatrix[0][genomeIndex] == 0)
+                                {
+                                  differentScores[6]++;
+                                  cout << "hehe " << differentScores[6] << endl;
+                                }
                         }
                         else if (testMatrix[methodLine][genomeIndex] == 0)//false positive
                         {
@@ -75,11 +80,13 @@ void ComparisonMatrix::create_comparison()
                                 differentScores[5]++;
                         }
                 }
-                final_comparison_score_vector[methodLine].success = (differentScores[1]/matrixHeight);
-                final_comparison_score_vector[methodLine].falsePositive = (differentScores[2]/matrixHeight);
-                final_comparison_score_vector[methodLine].falseNegative = (differentScores[3]/matrixHeight);
-                final_comparison_score_vector[methodLine].tooMuchKmer = (differentScores[4]/matrixHeight);
-                final_comparison_score_vector[methodLine].notEnoughKmer = (differentScores[5]/matrixHeight);
+                final_comparison_score_vector[methodLine].success = (long double) differentScores[1]/matrixHeight;
+                final_comparison_score_vector[methodLine].falsePositive = (long double) differentScores[2]/matrixHeight;
+                final_comparison_score_vector[methodLine].falseNegative = (long double) differentScores[3]/matrixHeight;
+                final_comparison_score_vector[methodLine].tooMuchKmer = (long double) differentScores[4]/matrixHeight;
+                final_comparison_score_vector[methodLine].notEnoughKmer = (long double) differentScores[5]/matrixHeight;
+                final_comparison_score_vector[methodLine].zeroHit = differentScores[6];
+
         }
 }
 
@@ -87,18 +94,46 @@ void ComparisonMatrix::create_comparison()
 void ComparisonMatrix::show_the_matrix()
 {
   long unsigned int methodLine(0);//this type because it's compared to size()
+  char space = ' ';
   for (methodLine = 1; methodLine < testMatrix.size(); methodLine++)
   {
-        cout << "For the method " << methodLine << endl;
-        cout << "Number of:" << endl << endl <<  "   matches   ";
-        cout << final_comparison_score_vector[methodLine].success << endl;
+        cout << endl << endl << endl << "Scores comparison with the Test table For the method " << methodLine << endl << "*************************************************" << endl;
+        cout << "For " << matrixHeight << " genomes , percent of:" << endl << endl <<  "   Matches           ";
+        cout << final_comparison_score_vector[methodLine].success * 100 << " %" << string(20 - get_number_digits(final_comparison_score_vector[methodLine].success * 100), space) << "    (" << final_comparison_score_vector[methodLine].success * matrixHeight << " genomes qualified)     (Full no hits: " << final_comparison_score_vector[methodLine].zeroHit << ")" << endl;
         cout << endl << endl <<  "   False positives   ";
-        cout << final_comparison_score_vector[methodLine].falsePositive << endl;
+        cout << final_comparison_score_vector[methodLine].falsePositive * 100 << " %" << string(20 - get_number_digits(final_comparison_score_vector[methodLine].falsePositive * 100), space) << "    (" << final_comparison_score_vector[methodLine].falsePositive * matrixHeight << " genomes qualified)" <<  endl;
         cout << endl << endl <<  "   False negatives   ";
-        cout << final_comparison_score_vector[methodLine].falseNegative << endl;
-        cout << endl << endl <<  "   with more kmer   ";
-        cout << final_comparison_score_vector[methodLine].tooMuchKmer << endl;
-        cout << endl << endl <<  "   with less kmer   ";
-        cout << final_comparison_score_vector[methodLine].notEnoughKmer << endl;
+        cout << final_comparison_score_vector[methodLine].falseNegative * 100 << " %" << string(20 - get_number_digits(final_comparison_score_vector[methodLine].falseNegative * 100), space) << "    (" << final_comparison_score_vector[methodLine].falseNegative * matrixHeight << " genomes qualified)" <<  endl;
+        cout << endl << endl <<  "   With more kmer    ";
+        cout << final_comparison_score_vector[methodLine].tooMuchKmer * 100 << " %" << string(20 - get_number_digits(final_comparison_score_vector[methodLine].tooMuchKmer * 100), space) << "    (" << final_comparison_score_vector[methodLine].tooMuchKmer * matrixHeight << " genomes qualified)" <<  endl;
+        cout << endl << endl <<  "   With less kmer    ";
+        cout << final_comparison_score_vector[methodLine].notEnoughKmer * 100 << " %" << string(20 - get_number_digits(final_comparison_score_vector[methodLine].notEnoughKmer * 100), space) << "    (" << final_comparison_score_vector[methodLine].notEnoughKmer * matrixHeight << " genomes qualified)" <<  endl << endl << "*************************************************" << endl << endl;
   }
+}
+
+
+
+
+int ComparisonMatrix::get_number_digits(long double aNumber)//use for number spaces for show the matrix
+{
+    int digits(0), limit(0);
+    while ( (int) aNumber != 0)
+    {
+      aNumber /= 10;
+      digits++;
+    }
+    while (aNumber - (int) aNumber && limit != 5 && aNumber != 0)
+    {
+      aNumber *=10;
+      digits++;
+      limit++;
+    }
+    if (digits==0)
+    {
+      return 1;
+    }
+    else
+    {
+      return digits;
+    }
 }
